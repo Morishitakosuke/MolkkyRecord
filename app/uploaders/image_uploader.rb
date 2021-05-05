b73bcd1b8f883abc113ab4e6c35cd2ecca374904
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-05 23:32:22 +0900

    rubocop: コード保守のため

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,9 +31,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fill => [50, 50, gravity = ::Magick::CenterGravity]
-  end
+  # version :thumb do
+  #   process :resize_to_fill => [50, 50, gravity = ::Magick::CenterGravity]
+  # end
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
