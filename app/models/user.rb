46602f98a35501d9cb68fed86f5217bcd4449752
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-05 23:46:28 +0900

    create: postモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts, dependent: :destroy
+
   validates :username, presence: true, length: { maximum: 12 }
   validates :introduction, length: { maximum: 250 }
 
