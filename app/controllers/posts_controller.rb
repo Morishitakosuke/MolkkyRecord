45e4c8dd53e49a32cd61800c90eba35146b9184e
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-06 21:37:15 +0900

    add: postsコントローラにcreateアクション,追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿しました！"
+      redirect_to user_path(current_user)
+    else
+      render new_post_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content,:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 200 }
+
+  mount_uploader :avatar, ImageUploader
 end
