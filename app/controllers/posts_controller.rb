41e8d80a49f03149f8f47055968f38e7acd3f328
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-09 23:00:46 +0900

    layout: postのshowページのレイアウトを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :post_current_user, only: [:edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -34,14 +35,24 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post.destroy
-    flash[:success] = "つぶやきを削除しました！"
-    redirect_to user_path(current_user)
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to root_path
+      flash[:success] = "つぶやきを削除しました！"
+    else
+      render :show
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def post_current_user
+    @posts = current_user.posts
+    @post = @posts.find_by(id: params[:id])
+    redirect_to new_post_path unless @post
   end
 end
