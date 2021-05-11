cb413bf7c09b4c3ec2a0c12afd66657c256f040d
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-11 20:06:59 +0900

    test: postsの統合テスト追加

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -51,4 +51,42 @@ RSpec.describe '新規投稿', type: :feature do
   end
 end
 
-
+RSpec.describe '投稿内容の編集', type: :feature do
+  before do
+    @user = FactoryBot.build(:user)
+    @post = FactoryBot.create(:post)
+  end
+  context '投稿内容が編集できるとき' do
+    it 'ログインしたユーザーは、自分が投稿した投稿内容を編集ができる' do
+      # 投稿を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      click_button('ログインする') 
+      # 投稿の詳細ページへ遷移する
+      visit post_path(@post.id)
+      # 編集するボタンをクリックし、編集ページへ遷移する
+      find_link('編集').click
+      expect(current_path).to eq edit_post_path(@post)
+      # すでに投稿済みの内容がフォームに入っている
+      expect(find('#post_content').value).to eq @post.content
+      # 投稿内容を編集する
+      image_path = Rails.root.join('app/assets/images/test.jpg')
+      fill_in 'post_content', with: "#{@post.content}+編集OK!"
+      # 編集してもPostモデルのカウントは変わらない
+      expect { click_on('更新する') }.to change(Post, :count).by(0)
+      # ユーザーページには編集した内容の投稿が存在する
+      expect(page).to have_content("#{@post.content}+編集OK!")
+    end
+  end
+  context '投稿内容が編集できないとき' do
+    it 'ログインしていないと、投稿の編集画面には遷移できない' do
+      # トップページにいる
+      visit root_path
+      # 投稿の詳細ページへ遷移する
+      visit post_path(@post)
+      # 投稿に編集ボタンがない
+      expect(page).to have_no_link '編集', href: edit_post_path(@post)
+    end
+  end
+end
