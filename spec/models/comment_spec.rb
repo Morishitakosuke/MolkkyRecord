086f250949770180de916cb5f8beea8d350589d7
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-12 20:01:08 +0900

    test: コメントのモデルテスト作成

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメントの保存' do
+    context 'コメントが保存できる場合' do
+      it 'テキストがあれば保存される' do
+        expect(@comment).to be_valid
+      end
+    end
+  end
+
+  context 'コメントが保存できない場合' do
+    it 'テキストが空だとコメントは保存できない' do
+      @comment.content = ''
+      @comment.valid?
+    end
+
+    it 'ユーザーが紐付いていないとコメントは保存できない' do
+      @comment.user = nil
+      @comment.valid?
+    end
+
+    it 'コメントされている投稿が紐付いていないとコメントは保存できない' do
+      @comment.post = nil
+      @comment.valid?
+    end
+  end
 end
