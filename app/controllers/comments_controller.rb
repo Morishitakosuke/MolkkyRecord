7d7405b58cf2c06239cb182f8da942f58480d1bd
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-12 22:17:31 +0900

    add: コメントコントローラにdestroyアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :comment_current_user, only: :destroy
 
   def create
     @comment = Comment.new(comment_params)
@@ -13,9 +14,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_back(fallback_location: root_path)
+      flash[:success] = "コメントを削除しました！"
+    else
+      flash[:danger] = "コメントに削除に失敗しました。"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
   private
 
-    def comment_params
-      params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
-    end
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def comment_current_user
+    @comments = current_user.comments
+    @comment = @comments.find_by(id: params[:id])
+  end
 end
