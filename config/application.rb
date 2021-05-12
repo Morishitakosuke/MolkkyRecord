4cf0ee71781937c516ec6ba9622984c57568b1e8
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-12 19:50:43 +0900

    add: rspecの設定追加

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module MolkkyRecord
       g.test_framework :rspec, 
             view_specs: false, 
             helper_specs: false, 
-            controller_specs: false, 
+            controller_specs: false,
+            request_specs: false,
             routing_specs: false
     end
   end
