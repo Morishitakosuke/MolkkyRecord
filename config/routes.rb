0f230d45cd8fd54e5d5087d6059e0be675122368
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-13 17:56:25 +0900

    add: relationshipsのルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     passwords: 'users/passwords',
   }
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    member do
+      get :following, :followers
+    end
+  end
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
 end
