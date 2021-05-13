97ad48dcd44c84728ddf6181945157aa91f77358
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-13 18:06:25 +0900

    add: relationshipsコントローラにメソッド追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,21 @@
 class RelationshipsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    return if current_user.id == params[:followed_id]
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+  end
+
+  protected
+    def logged_in_user
+      return if user_signed_in?
+      flash[:danger] = "アカウント登録もしくはログインしてください。"
+      redirect_to new_user_session_path
+    end
 end
