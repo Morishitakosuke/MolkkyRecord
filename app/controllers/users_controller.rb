579a36f71de7cb1a1dc5ac3c6d073f927b94a52a
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-15 00:20:13 +0900

    add: フォロー機能のコントローラ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = Post.page(params[:page]).per(6).order('updated_at DESC').where(user_id: @user.id)
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.followings.page(params[:page]).per(5)
+    render 'show_followings'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.page(params[:page]).per(5)
+    render 'show_followers'
+  end
 end
