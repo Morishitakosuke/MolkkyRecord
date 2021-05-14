108415223e06bd5cb9ed0be83e00656ea456ae55
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-14 22:28:59 +0900

    add: フォロー機能のルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  resources :follow_relationships, only: [:create, :destroy]
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
-  resources :relationships, only: [:create, :destroy]
 end
