4daaa8658cc90ece4f46758366ef68623d529f26
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-18 20:15:39 +0900

    add: 通知機能のモデルのメソッド作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  # パスワード無しで編集可
   def update_without_current_password(params, *options)
     params.delete(:current_password)
     if params[:password].blank? && params[:password_confirmation].blank?
@@ -29,19 +30,35 @@ class User < ApplicationRecord
     result
   end
 
+  # すでにフォロー済みであればtrue返す
   def following?(other_user)
     followings.include?(other_user)
   end
 
+  # ユーザーをフォローする
   def follow(other_user)
     following_relationships.create(following_id: other_user.id)
   end
 
+  # フォローを解除する
   def unfollow(other_user)
     following_relationships.find_by(following_id: other_user.id).destroy
   end
 
+  # いいねされているか確認
   def already_liked?(post)
     likes.exists?(post_id: post.id)
+  end
+
+  # フォロー通知
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
   end
 end
