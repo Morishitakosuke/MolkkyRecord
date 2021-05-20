1fbfba080e6ed94686b76a0b6b5a61b68f5fa45b
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-20 22:58:02 +0900

    add: タグ機能のモデルへの保存のメソッド作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -49,4 +49,26 @@ class Post < ApplicationRecord
     end
     notification.save if notification.valid?
   end
+
+  # タグ機能
+  after_create do
+    post = Post.find_by(id: self.id)
+    tags  = self.caption.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+    post.tags = []
+    tags.uniq.map do |tag|
+      #タグは先頭の'#'を外した上で保存
+      tag = Tag.find_or_create_by(name: tag.downcase.delete('#'))
+      post.tags << tag
+    end
+  end
+
+  before_update do 
+    post = Post.find_by(id: self.id)
+    post.tags.clear
+    tags = self.caption.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+    tags.uniq.map do |tag|
+      tag = Tag.find_or_create_by(name: tag.downcase.delete('#'))
+      post.tags << tag
+    end
+  end
 end
