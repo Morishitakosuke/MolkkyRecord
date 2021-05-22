24652c0cc2d89c99dae10dc7da885d99e46b5364
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-23 01:47:33 +0900

    rubocop: コード保守のため

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  validates :name, presence: true, length: { maximum:100}
+  validates :name, presence: true, length: { maximum: 100 }
   has_many :tag_post, dependent: :destroy
   has_many :post, through: :tag_post
 end
