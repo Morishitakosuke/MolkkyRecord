c1b95b24e64af69c936ded75572bdb4d8caea466
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-05-22 19:05:32 +0900

    test: タグ機能のモデルテスト作成

--- spec/models/tag_post_spec.rb
+++ spec/models/tag_post_spec.rb
@@ -1,5 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe TagPost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe TagPost do
+  describe "アソシエーション" do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context "Postモデルとの関係" do
+      let(:target) { :post }
+
+      it "多:1" do
+        expect(association.macro).to eq :belongs_to
+      end
+
+      it "結合するモデルのクラス名：Post" do
+        expect(association.class_name).to eq "Post"
+      end
+    end
+
+    context "Tagモデルとの関係" do
+      let(:target) { :tag }
+
+      it "多:1" do
+        expect(association.macro).to eq :belongs_to
+      end
+
+      it "結合するモデルのクラス名：Tag" do
+        expect(association.class_name).to eq "Tag"
+      end
+    end
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Tag do
+  describe "アソシエーション" do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context "Postモデルとの関係" do
+      let(:target) { :post }
+
+      it "1:多" do
+        expect(association.macro).to eq :has_many
+      end
+
+      it "結合するモデルのクラス名：Post" do
+        expect(association.class_name).to eq "Post"
+      end
+    end
+
+    context "TagPostモデルとの関係" do
+      let(:target) { :tag_post }
+
+      it "1:多" do
+        expect(association.macro).to eq :has_many
+      end
+
+      it "結合するモデルのクラス名：TagPosts" do
+        expect(association.class_name).to eq "TagPost"
+      end
+    end
+  end
 end
