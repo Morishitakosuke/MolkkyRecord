70edbd95dea1935b18e02c86e637e87f1e8c2014
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-06-03 20:58:23 +0900

    add: コントローラに制限追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,10 @@
 class RecordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_record, only: [:update, :destroy]
+
+  private
+
+  def set_record
+    @score_record = Record.find_by(user_id: current_user.id)
+  end
 end
