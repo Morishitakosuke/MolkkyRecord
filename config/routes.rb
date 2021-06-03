f98604329fac8a32286be627c3b639a2043496e2
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-06-03 20:51:31 +0900

    add: スコア機能のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end 
   resources :users, only: [:show] do
+    resources :records
     member do
       get :following, :followers
     end
