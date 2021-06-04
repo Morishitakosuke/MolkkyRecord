7867d2128a6b4b36dfe064d287443b112a1f0cfa
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-06-04 19:51:53 +0900

    add: スコア機能のモデルのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_142526) do
+ActiveRecord::Schema.define(version: 2021_06_04_104156) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2021_05_31_142526) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "enemyteam"
+    t.string "enemyname"
     t.index ["user_id"], name: "index_records_on_user_id"
   end
 
