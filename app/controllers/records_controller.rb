183545590d114c7e06f4240bf66a771b2456c319
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-06-14 00:12:39 +0900

    fix: マジックナンバーを定数化

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -22,8 +22,8 @@ class RecordsController < ApplicationController
     @records = Record.where(id: params[:id])
     @total_score = 0
     # モルックのルール上、投げる回数が5回以下にはならない
-    @over_score = 25 + @record.score_6 + @record.score_7 + @record.score_8 + @record.score_9 + @record.score_10
-    @over_enemyscore = 25 + @record.enemyscore_6 + @record.enemyscore_7 +
+    @over_score = Const::OVER_FIFTY_SCORE + @record.score_6 + @record.score_7 + @record.score_8 + @record.score_9 + @record.score_10
+    @over_enemyscore = Const::OVER_FIFTY_SCORE + @record.enemyscore_6 + @record.enemyscore_7 +
       @record.enemyscore_8 + @record.enemyscore_9 + @record.enemyscore_10
     @records.each do |record|
       @total_enemyscore = record.enemyscore_1 + record.enemyscore_2 + record.enemyscore_3 +
---
+++ config/initializers/constants.rb
@@ -0,0 +1,5 @@
+module Const
+  # スコアの定数
+  WIN_SCORE = 50
+  OVER_FIFTY_SCORE = 25
+end
